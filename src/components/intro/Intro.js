2beb76433a0e63be66ca3c15f456193764d0610e
Dimitrios Tsikopoulos <dimitristsik@gmail.com>
2021-12-26 13:11:44 +0200

    Intro done

--- src/components/intro/Intro.js
+++ src/components/intro/Intro.js
@@ -1,9 +1,41 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
+import { init } from "ityped";
 
 import "./intro.scss";
 
 const Intro = () => {
-  return <div className="intro" id="intro"></div>;
+  const textRef = useRef();
+
+  useEffect(() => {
+    init(textRef.current, {
+      showCursor: true,
+      backDelay: 1500,
+      backSpeed: 60,
+      strings: ["Software Engineer", "Designer", "Web Developer"],
+    });
+  }, []);
+
+  return (
+    <div className="intro" id="intro">
+      <div className="left">
+        <div className="imgContainer">
+          <img src="assets/man.png" alt="man"></img>
+        </div>
+      </div>
+      <div className="right">
+        <div className="wrapper">
+          <h2>Hi There, I'm</h2>
+          <h1>Dimitris Tsikopoulos</h1>
+          <h3>
+            I'm a <span ref={textRef}></span>
+          </h3>
+        </div>
+        <a href="#portfolio">
+          <img src="assets/down.png"></img>
+        </a>
+      </div>
+    </div>
+  );
 };
 
 export default Intro;
