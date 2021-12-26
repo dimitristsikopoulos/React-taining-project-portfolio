9d24547232039ab2ce05dbf29ede2c6083504b0f
Dimitrios Tsikopoulos <dimitristsik@gmail.com>
2021-12-26 09:56:17 +0200

    Finished hamburger menu

--- src/components/topbar/Topbar.js
+++ src/components/topbar/Topbar.js
@@ -3,9 +3,9 @@ import { Person, Mail } from "@material-ui/icons";
 
 import "./topbar.scss";
 
-const Topbar = () => {
+const Topbar = ({ menuOpen, setMenuOpen }) => {
   return (
-    <div className="topbar">
+    <div className={"topbar " + (menuOpen && "active")}>
       <div className="wrapper">
         <div className="left">
           <a href="#intro" className="logo">
@@ -20,7 +20,13 @@ const Topbar = () => {
             <span>dimitris@dimitris.com</span>
           </div>
         </div>
-        <div className="right"></div>
+        <div className="right">
+          <div className="hamburger" onClick={() => setMenuOpen(!menuOpen)}>
+            <span className="line1"></span>
+            <span className="line2"></span>
+            <span className="line3"></span>
+          </div>
+        </div>
       </div>
     </div>
   );
